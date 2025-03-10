4e95374b88d4005c893548603d0f2fdfd1e9a221
Pipetto-crypto <72308298+Pipetto-crypto@users.noreply.github.com>
2025-03-10 07:56:53 +0100

    MainActivity: Create proton AppData directory

--- app/src/main/java/com/micewine/emu/activities/MainActivity.kt
+++ app/src/main/java/com/micewine/emu/activities/MainActivity.kt
@@ -1051,8 +1051,10 @@ class MainActivity : AppCompatActivity() {
                 File("$appRootDir/wine-utils/CoreFonts").copyRecursively(File("$winePrefix/drive_c/windows/Fonts"), true)
 
                 /* It's safe to assume that if this folder doesn't exist we are using Proton */
-                if (!localAppData.exists())
+                if (!localAppData.exists()) {
                     localAppData = File("$driveC/users/steamuser/AppData")
+                    localAppData.mkdirs()
+                }    
 
                 localAppData.copyRecursively(File("$userSharedFolder/AppData"))
                 localAppData.deleteRecursively()
